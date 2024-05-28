a5dfb8b60d8fcad1aad84f94a630cdc598792f63
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2024-05-28 21:21:55 +0600

    first commit

---
+++ class2.py
@@ -0,0 +1,90 @@
+# print("Hello, World!")
+
+"""
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+"""
+
+# print("Hello, World!")
+# print("Hello, World!")
+
+
+# x = 5
+# y = "John" 
+# print(x)
+# print(y)
+
+# x = 100 
+# y = 5000 
+# name = 'Rokon'
+# name1 = "Rokon"
+# name2 = "Sumon"
+
+# myvar = "John" ,"Sumon"
+
+# print(type(myvar))
+# print(name)
+# print(x + y , name)
+# print(x + y , name1)
+# print(x + y , name2)
+
+# myvar = "John" ,"Sumon"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR1 = "John"
+# myvar2 = "John"
+
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+# &myvar = "John"
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x) 
+
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+
+x = "Hello World 46 hvh$ "
+y = 100000
+z = 10.10
+a = 1000J
+
+print(type(a))
+
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
+
+
+
